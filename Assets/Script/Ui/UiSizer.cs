fec2b46314df33208a8abcb74eeec32ce2c21160
MatorMirne <mirnemator@gmail.com>
2022-10-28 12:01:08 +0900

    해상도 설정완료

---
+++ Assets/Script/Ui/UiSizer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiSizer : MonoBehaviour
+{
+    public bool sizing_per_width;
+    public bool sizing_per_height;
+    public float size_width;
+    public float size_height;
+    public float width;
+    public float height;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Sizing() { 
+        if (sizing_per_width)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
